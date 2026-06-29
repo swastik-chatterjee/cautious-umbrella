6fada213c000d9af68e9ea079269e9e509766b0f
swastik-chatterjee <swastik.chatterjee2014@zohomail.in>
2026-06-29 17:59:04 +0530

    Add NativeScript configuration with package ID com.swastik.wt

---
+++ nativescript.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projectName: 'WeightTracker',
+  appPath: 'app',
+  appResourcesPath: 'app/App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none'
+  },
+  id: 'com.swastik.wt',
+  displayName: 'KG Weight Tracker'
+};
